accc59c82f5d5b090568dd0feed6f82d24b3cfc1
hill0ne <jeanj531@gmail.com>
2022-07-31 19:44:49 +0900

    Refactor: "수정 슬라이드 밀림 현상"
    - 슬라이드 밀림 현상 수정
    - 가독성을 위한 파일 정리
    - 코드 가독성을 위한 gnb 메뉴 애니메이션 함수 수정

--- js/main.js
+++ js/main.js
@@ -1,24 +1,63 @@
 const main = document.querySelector(".main");
-const gnblist = document.querySelector(".list");
-const gnbDropDown = document.querySelector(".gnb-menu-dropdown");
-const specialHallList = document.querySelector(".specialHall-list");
+const gnb = document.querySelector(".gnb_menu");
+const gnb_list = document.querySelector(".gnb_list");
+const gnbDropDown = document.querySelector(".gnb_menu_dropdown");
+const personalMenu = document.querySelectorAll(".personal_menu_wrap li");
+const specialHallList = document.querySelector(".specialHall_list");
 const checked = document.querySelector(".list-checked");
-const image = document.querySelector(".specialHall-image");
-let li = specialHallList.children;
+const image = document.querySelector(".specialHall_image");
+const nextBtn = document.querySelector(".button_next");
+const nextBtnActive = document.querySelector(".button_next .is-active");
 
-// dropdown menu
+const gnb_li = gnb_list.children;
 
-gnblist.addEventListener("mouseover", () => {
+let buttonStatus = false;
+let special_li = specialHallList.children;
+let a = null;
+
+// tab 으로 gnb 메뉴 열기
+for (item of gnb_li) {
+  a = item.firstElementChild;
+  a.addEventListener("keydown", (e) => {
+    if (e.keyCode === 13) {
+      console.log("13");
+      openDropDownMenu();
+    }
+  });
+}
+
+// 마우스 호버로 gnb 메뉴 열기
+gnb.addEventListener("mouseover", () => {
+  openDropDownMenu();
+});
+
+gnbDropDown.addEventListener("mouseover", () => {
+  openDropDownMenu();
+});
+
+gnbDropDown.addEventListener("mouseout", () => {
+  closeDropDownMenu();
+});
+
+gnb.addEventListener("mouseout", () => {
+  closeDropDownMenu();
+});
+
+function openDropDownMenu() {
   gnbDropDown.classList.add("is-active");
-});
+}
 
-gnblist.addEventListener("mouseout", () => {
+function closeDropDownMenu() {
   gnbDropDown.classList.remove("is-active");
-});
+}
 
-// specialHall switch image
+// 모달창
+for (item of personalMenu) {
+  item.addEventListener("click", showInfoPage);
+}
 
-const Image = [
+// specialHall 이미지 변경
+const Images = [
   {
     id: "1",
     src: "./assets/img/special-suitecinema.png",
@@ -37,19 +76,19 @@ const Image = [
   },
 ];
 
-for (const item of li) {
+for (const item of special_li) {
   const a = item.firstElementChild;
   const span = item.firstElementChild;
   const dataID = a.dataset.id;
   item.addEventListener("mouseover", () => {
-    image.src = Image[dataID].src;
+    image.src = Images[dataID].src;
     checked.classList.remove("list-checked");
     item.classList.add("list-checked");
     span.style.cssText = `border: none`;
   });
 }
 
-for (const item of li) {
+for (const item of special_li) {
   item.addEventListener("mouseleave", () => {
     const span = item.firstElementChild;
     item.classList.remove("list-checked");
@@ -58,11 +97,49 @@ for (const item of li) {
   });
 }
 
-// move top
+// fixed button 노출
+const fixButton = document.querySelector(".fixed_button");
+let scroll = 0;
 
+document.addEventListener("scroll", () => {
+  scroll = document.documentElement.scrollTop;
+  if (scroll > 380) {
+    showFixedButon();
+  } else {
+    hideFixedButton();
+  }
+});
+
+function showFixedButon() {
+  fixButton.classList.add("is-active");
+}
+
+function hideFixedButton() {
+  fixButton.classList.remove("is-active");
+}
+
+// 슬라이드 버튼
+nextBtn.addEventListener("click", slideMoveNext);
+
+function slideMoveNext() {
+  console.log("넥스트");
+  if (buttonStatus === false) {
+    nextBtn.classList.add("is-active");
+    buttonStatus = true;
+  } else if (buttonStatus === true) {
+    console.log("프리브");
+  }
+}
+
+// top 버튼
 const gotoTopButton = document.querySelector(".gotoTop-button");
 gotoTopButton.addEventListener("click", moveTop);
 
 function moveTop() {
-  window.scrollTo(0, 0);
+  window.scrollTo({ top: 0, behavior: "smooth" });
+}
+
+// 모달창
+function showInfoPage() {
+  console.log("준비 페이지 입니다.");
 }
